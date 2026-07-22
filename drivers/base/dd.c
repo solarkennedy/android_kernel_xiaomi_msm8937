0264f72de33e3396330211afa79398bd07e67b5b
Kyle Anderson <kyle@cascade.family>
2026-07-22 09:53:06 -0700

    dd.c: restore base whitespace (drop bring-up revert artifact)
    The pr_info revert (9df7ce0e) left a one-space indentation change on the
    really_probe continuation line. Restore dd.c to the lineage-23.2 base.
    
    Change-Id: I02e0cb9ae42f616b255cade793ca540064f9f09a

--- drivers/base/dd.c
+++ drivers/base/dd.c
@@ -501,7 +501,7 @@ static int really_probe(struct device *dev, struct device_driver *drv)
 
 	atomic_inc(&probe_count);
 	pr_debug("bus: '%s': %s: probing driver %s with device %s\n",
-		drv->bus->name, __func__, drv->name, dev_name(dev));
+		 drv->bus->name, __func__, drv->name, dev_name(dev));
 	if (!list_empty(&dev->devres_head)) {
 		dev_crit(dev, "Resources present before probing\n");
 		ret = -EBUSY;
